rostag/bigpolicy_eu

feature(Material2): Enable Angular-Material2 (#26)
* feature(proj): Enable Material2 library access

* feature(navbar): Materialize toolbar/ nav and fix tests

* chore(travis): Update minimum Node version to 5

* feature(home): Home component and fix tests

* feature(about): About component and fix tests

src/client/app/+home/home.component.ts
@@ -1,5 +1,9 @@
 import { FORM_DIRECTIVES } from '@angular/common';
 import { Component } from '@angular/core';
+import {MdButton} from '@angular2-material/button';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 
 import { NameListService } from '../shared/index';
 
@@ -11,7 +15,7 @@ import { NameListService } from '../shared/index';
   selector: 'sd-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
-  directives: [FORM_DIRECTIVES]
+  directives: [FORM_DIRECTIVES, MdButton, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, MD_LIST_DIRECTIVES]
 })
 export class HomeComponent {
 

RoxKilly/angular2-seed-material2

src/client/app/+home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
+import { Component } from '@angular/core';
+import {MdButton} from '@angular2-material/button';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 
 import { NameListService } from '../shared/index';
 
@@ -11,7 +16,7 @@ import { NameListService } from '../shared/index';
   selector: 'sd-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
-  directives: [REACTIVE_FORM_DIRECTIVES]
+  directives: [REACTIVE_FORM_DIRECTIVES, MdButton, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, MD_LIST_DIRECTIVES]
 })
 export class HomeComponent {
 